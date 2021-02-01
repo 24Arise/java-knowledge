14ca8dfbf4988977a252f40dfc3e03c8b8aa7571
24Arise <tagin@live.cn>
2021-02-01 17:30:01 +0800

    [IMP] 1、调整目录，将 JsonDeserialize 抽离为 module ；2、增加 02-zip module 用来学习 Java 中 zip 压缩

---
+++ 01-jsondeserialize/src/main/java/com/arise/jsondeserialize/enitty/Person.java
@@ -0,0 +1,38 @@
+package com.arise.jsondeserialize.enitty;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.org.arise.JsonDeserialize.service.OptimizedBooleanDeserializer;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * <p>
+ * Person 实体类
+ * </p>
+ *
+ * @author 24Arise 2020/12/06 15:23
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Person {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date birthday;
+
+	@JsonDeserialize(using = OptimizedBooleanDeserializer.class)
+	private Boolean enabled;
+
+}
