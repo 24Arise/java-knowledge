87083b660f637968073c907db91c53f6cd4618fa
24Arise <tagin@live.cn>
2021-02-02 13:27:50 +0800

    [DEL] 删除无用的 包 引入

--- 01-jsondeserialize/src/test/java/com/arise/jsondeserialize/JsondeserializeApplicationTests.java
+++ 01-jsondeserialize/src/test/java/com/arise/jsondeserialize/JsondeserializeApplicationTests.java
@@ -4,7 +4,6 @@ import com.arise.jsondeserialize.enitty.Person;
 import com.arise.jsondeserialize.service.IJsonDeserializeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
